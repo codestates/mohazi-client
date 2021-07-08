16de5345df97044146a73d229a57893f20f37185
Franchesca Kim <franchescakim09@gmail.com>
2021-07-09 01:33:44 +0900

    Toggle initially set depending on the User's current time

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../actions/actions.js';
 import axios from 'axios';
 import LoginModal from './Modals/Login';
+import { useEffect } from "react";
 
 require("dotenv").config();
 const server = process.env.REACT_APP_SERVER_URL;
@@ -86,7 +87,7 @@ const LogoutButton = styled(Link)`
 
 const Day_Night_Toggle = styled.img`
     cursor: pointer;
-    margin: 0 10px;
+    margin: 0 20px;
 `
 
 function Header() {
@@ -94,10 +95,19 @@ function Header() {
     const dispatch = useDispatch();
     const state = useSelector(state => state);
     let { isLogin } = state;
-    
+
     const dayToggleUrl = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiPjxwYXRoIGQ9Ik02IDE4aDEyYzMuMzExIDAgNi0yLjY4OSA2LTZzLTIuNjg5LTYtNi02aC0xMi4wMzljLTMuMjkzLjAyMS01Ljk2MSAyLjcwMS01Ljk2MSA2IDAgMy4zMTEgMi42ODggNiA2IDZ6bTEyLTEwYy0yLjIwOCAwLTQgMS43OTItNCA0czEuNzkyIDQgNCA0IDQtMS43OTIgNC00LTEuNzkyLTQtNC00eiIvPjwvc3ZnPg==";
     const nightToggleUrl = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiPjxwYXRoIGQ9Ik0xOCAxOGgtMTJjLTMuMzExIDAtNi0yLjY4OS02LTZzMi42ODktNiA2LTZoMTIuMDM5YzMuMjkzLjAyMSA1Ljk2MSAyLjcwMSA1Ljk2MSA2IDAgMy4zMTEtMi42ODggNi02IDZ6bS0xMi0xMGMyLjIwOCAwIDQgMS43OTIgNCA0cy0xLjc5MiA0LTQgNC00LTEuNzkyLTQtNCAxLjc5Mi00IDQtNHoiLz48L3N2Zz4=";
-    const [toggleUrl, setToggleUrl] = useState(dayToggleUrl);
+    
+    const date = new Date();
+    const hour = date.getHours();
+    let initialToggleUrl = '';
+    if(hour >= 6 && hour < 18) {
+        initialToggleUrl = dayToggleUrl;
+    } else {
+        initialToggleUrl = nightToggleUrl;
+    }
+    const [toggleUrl, setToggleUrl] = useState(initialToggleUrl);
 
     function handleLogout() {
         if(confirm("로그아웃 하시겠습니까?") === true) {
@@ -124,17 +134,23 @@ function Header() {
 
     function handleToggle(event) {
         if(event.target.name === 'day') {
-            SetBackgroundColor('RGB(26,36,54)');
-            SetTextColor('white');
             setToggleUrl(nightToggleUrl);
             event.target.name = 'night'
         } else {
-            SetBackgroundColor('white');
-            SetTextColor('black');
             setToggleUrl(dayToggleUrl);
             event.target.name = 'day'
         }
     }
+
+    useEffect(() => {
+        if(toggleUrl === nightToggleUrl){
+            SetBackgroundColor('RGB(26,36,54)');
+            SetTextColor('white');
+        } else if(toggleUrl === dayToggleUrl) {
+            SetBackgroundColor('white');
+            SetTextColor('black');
+        }
+    }, [toggleUrl])
     
     return (
         <FixPosition>
